f2a44c0ec666ae9e00a96637cc19798ae942feec
jharashchuk <j.garaschuk@gmail.com>
2022-03-10 19:38:28 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if(str.length % 2 != 0) return false;
+
+  let arr = [];
+  let obj = Object.fromEntries(bracketsConfig);
+
+  for (let elem of str.split('')) {
+    if (obj[arr[arr.length-1]] === elem) {
+      arr.pop(); 
+      } else if (Object.keys(obj).includes(elem)){ 
+        arr.push(elem); 
+      }
+  }
+    return arr.length === 0;
+};
